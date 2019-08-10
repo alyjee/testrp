796b1d065ac0f04e1b2c2443e455e4dde00dd5e9
Tahir Raza <tahirraza.se@gmail.com>
2019-08-10 20:12:39 +0500

    added import command

--- commands/ImportController.php
+++ commands/ImportController.php
@@ -1,46 +1,145 @@
 <?php
 namespace app\commands;
 
-use app\models\Loan;
-use app\models\User;
 use Yii;
 use yii\console\Controller;
+use \JsonMachine\JsonMachine;
 
 /**
  * This command imports the user.json and loan.json to DB.
  */
 class ImportController extends Controller
 {
-    public function actionIndex()
+    public $message;
+
+    public function options($actionID){
+        return ['message'];
+    }
+
+    public function optionAliasess()
     {
-        // User
-        $data = json_decode(file_get_contents(\Yii::$app->basePath.'/users.json'), true);
+        return ['m' => 'message'];
+    }
 
-        $columnNameArray=['id', 'first_name', 'last_name', 'email', 'personal_code', 'phone', 'active', 'dead', 'lang'];
-        $insertCount = Yii::$app->db->createCommand()
-            ->batchInsert(
-                'user', $columnNameArray, $data
-            )
-            ->execute();
+    public function actionIndex($model='User', $file='users.json')
+    {
+        echo "Import started for $model model @ ".date('Y-m-d H:i:s')."\n";
+        $class_name = 'app\\models\\'.$model;
+        $modelInstance = new $class_name();
 
-        echo $insertCount . ' users has been added.' . PHP_EOL;
-
-        // Loan
-        $data = json_decode(file_get_contents(\Yii::$app->basePath.'/loans.json'), true);
-
-        foreach ($data as &$loan) {
-            $loan['start_date'] = date('Y/m/d H:i:s', $loan['start_date']);
-            $loan['end_date'] = date('Y/m/d H:i:s', $loan['end_date']);
-            $loan['status'] = boolval(intval($loan['status']));
+        $columnSchema = [];
+        $columns = $modelInstance->getTableSchema()->columns;
+        foreach($columns as $column) {
+            $columnSchema[$column->name] = $column->type;
         }
+        
+        // Create a temporary table just like user
+        $baseTable = $modelInstance->getTableSchema()->fullName;
+        $tmpTable =  'temp_'.$modelInstance->getTableSchema()->fullName;
 
-        $columnNameArray = ['id', 'user_id', 'amount', 'interest', 'duration', 'start_date', 'end_date', 'campaign', 'status'];
-        $insertCount = Yii::$app->db->createCommand()
-            ->batchInsert(
-                'loan', $columnNameArray, $data
-            )
-            ->execute();
+        // DROP Table IF EXISTS
+        // Overide the dropTable function to support IF EXISTS option
+        // otherwise, it will throw an error becuase table is not existing on first time
+        // Yii::$app->db->createCommand()->dropTable($tmpTable)->execute();
+        Yii::$app->db->createCommand("DROP TABLE IF EXISTS $tmpTable")->execute();
+        
+        /*
+         * TODO: Overide the function createTable 
+         * in order to support UNLOGGED, IF NOT EXISTS options
+         *
+         */
+        $rawSql = Yii::$app->db->createCommand()->createTable($tmpTable, $columnSchema)->getRawSql();
+        $rawSql = str_replace('CREATE TABLE', 'CREATE UNLOGGED TABLE IF NOT EXISTS ', $rawSql);
+        Yii::$app->db->createCommand($rawSql)->execute();
+        
+        echo "Inserting data in temporary table...\n";
+        // Read the json file with stream
+        $users = JsonMachine::fromFile(Yii::$app->basePath.'/'.$file);
+        $userData = [];
+        foreach ($users as $id => $user) {
+            if( $id < 10){
+                $userData[] = $user;
+                // call batch insert on this
+            } else {
+                // send a batch of 100 records for insertion
+                $insertCount = Yii::$app->db->createCommand()
+                    ->batchInsert(
+                        $tmpTable, $modelInstance->getTableSchema()->columnNames, $userData
+                    )
+                    ->execute();
+                // empty the userData array after insetion
+                // and insert the current user in the array as a first element
+                $userData = [];
+                $userData[] = $user;
+            }
+        }
+        // insert the remaining data if any
+        if(count($userData) > 0)
+        {
+            $insertCount = Yii::$app->db->createCommand()
+                ->batchInsert(
+                    $tmpTable, $modelInstance->getTableSchema()->columnNames, $userData
+                )
+                ->execute();
+        }
+        echo "Data insertion in temp table is completed.\n";
+        $commaSepratedColumnNames =  implode(', ', $modelInstance->getTableSchema()->columnNames);
+        $rawSql = "INSERT INTO \"$baseTable\" ($commaSepratedColumnNames) SELECT temp.* FROM \"$tmpTable\" temp ON CONFLICT (\"id\") DO UPDATE SET first_name = excluded.first_name, last_name = excluded.last_name, email = excluded.email, personal_code = excluded.personal_code, phone = excluded.phone, active = excluded.active, dead = excluded.dead, lang = excluded.lang";
 
-        echo $insertCount . ' loans has been added.' . PHP_EOL;
+        echo "Inserting data in main table...\n";
+        Yii::$app->db->createCommand($rawSql)->execute();
+        echo "Data insertion in main table is completed.\n";
+
+        Yii::$app->db->createCommand("DROP TABLE IF EXISTS $tmpTable")->execute();
+        echo "Temporary table dropped.\n";
+        echo "Task completed @ ".date('Y-m-d H:i:s')."\n";
+        return;
+    }
+
+    public function actionGenerate($limit=5, $fileName='users.json')
+    {
+        $file = Yii::$app->basePath.'/'.$fileName;
+        $fhandler = fopen($file, 'w');
+        
+        // empty the file
+        fwrite($fhandler, NULL);
+
+        // add start of array and a new line
+        fwrite($fhandler, "[");
+        fwrite($fhandler, "\n");
+
+        fclose($fhandler);
+
+        $fhandler = fopen($file, 'a');
+        
+        for ($i=1; $i <= $limit ; $i++) {
+            $user = [
+                "id" => $i,
+                "first_name" => "$i-Ullalaa",
+                "last_name" => "Ziip",
+                "email" => "ulallaaa@mldllm.tt",
+                "personal_code" => "49005025465",
+                "phone" => "50170262",
+                "active"=> "1",
+                "dead" => "0",
+                "lang" => "est"
+            ];
+            $userString = json_encode($user);
+            fwrite($fhandler, $userString);
+            if( $i == $limit )
+            {
+                fwrite($fhandler, "\n");
+            } else
+            {
+                fwrite($fhandler, ",\n");
+            }
+        }
+        
+        fwrite($fhandler, "]");
+        fwrite($fhandler, "\n");
+
+        fclose($fhandler);
+
+        echo "File Generated.\n";
     }
 }
