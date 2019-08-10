2b11a36a5d91f0c5c978b351e27d354161f3b80a
Tahir Raza <tahirraza.se@gmail.com>
2019-08-11 00:47:28 +0500

    final touches on import command

--- commands/ImportController.php
+++ commands/ImportController.php
@@ -3,97 +3,52 @@ namespace app\commands;
 
 use Yii;
 use yii\console\Controller;
-use \JsonMachine\JsonMachine;
+use yii\db\Query;
+use app\helpers\ImportHelper;
 
 /**
  * This command imports the user.json and loan.json to DB.
  */
 class ImportController extends Controller
 {
-    public $message;
-
-    public function options($actionID){
-        return ['message'];
-    }
-
-    public function optionAliasess()
-    {
-        return ['m' => 'message'];
-    }
-
     public function actionIndex($model='User', $file='users.json')
     {
-        echo "Import started for $model model @ ".date('Y-m-d H:i:s')."\n";
-        $class_name = 'app\\models\\'.$model;
-        $modelInstance = new $class_name();
+        $className = 'app\\models\\'.$model;
+        $modelInstance = new $className();
 
-        $columnSchema = [];
-        $columns = $modelInstance->getTableSchema()->columns;
-        foreach($columns as $column) {
-            $columnSchema[$column->name] = $column->type;
-        }
-        
-        // Create a temporary table just like user
+        $columnNames = $modelInstance->getTableSchema()->columnNames;
         $baseTable = $modelInstance->getTableSchema()->fullName;
         $tmpTable =  'temp_'.$modelInstance->getTableSchema()->fullName;
 
-        // DROP Table IF EXISTS
-        // Overide the dropTable function to support IF EXISTS option
-        // otherwise, it will throw an error becuase table is not existing on first time
-        // Yii::$app->db->createCommand()->dropTable($tmpTable)->execute();
-        Yii::$app->db->createCommand("DROP TABLE IF EXISTS $tmpTable")->execute();
+        ImportHelper::beforeImport($model);
         
-        /*
-         * TODO: Overide the function createTable 
-         * in order to support UNLOGGED, IF NOT EXISTS options
-         *
-         */
-        $rawSql = Yii::$app->db->createCommand()->createTable($tmpTable, $columnSchema)->getRawSql();
-        $rawSql = str_replace('CREATE TABLE', 'CREATE UNLOGGED TABLE IF NOT EXISTS ', $rawSql);
-        Yii::$app->db->createCommand($rawSql)->execute();
+        ImportHelper::streamInsert($model, $file, $tmpTable, $columnNames);
         
-        echo "Inserting data in temporary table...\n";
-        // Read the json file with stream
-        $users = JsonMachine::fromFile(Yii::$app->basePath.'/'.$file);
-        $userData = [];
-        foreach ($users as $id => $user) {
-            if( $id < 10){
-                $userData[] = $user;
-                // call batch insert on this
-            } else {
-                // send a batch of 100 records for insertion
-                $insertCount = Yii::$app->db->createCommand()
-                    ->batchInsert(
-                        $tmpTable, $modelInstance->getTableSchema()->columnNames, $userData
-                    )
-                    ->execute();
-                // empty the userData array after insetion
-                // and insert the current user in the array as a first element
-                $userData = [];
-                $userData[] = $user;
-            }
-        }
-        // insert the remaining data if any
-        if(count($userData) > 0)
-        {
-            $insertCount = Yii::$app->db->createCommand()
-                ->batchInsert(
-                    $tmpTable, $modelInstance->getTableSchema()->columnNames, $userData
-                )
-                ->execute();
-        }
-        echo "Data insertion in temp table is completed.\n";
-        $commaSepratedColumnNames =  implode(', ', $modelInstance->getTableSchema()->columnNames);
-        $rawSql = "INSERT INTO \"$baseTable\" ($commaSepratedColumnNames) SELECT temp.* FROM \"$tmpTable\" temp ON CONFLICT (\"id\") DO UPDATE SET first_name = excluded.first_name, last_name = excluded.last_name, email = excluded.email, personal_code = excluded.personal_code, phone = excluded.phone, active = excluded.active, dead = excluded.dead, lang = excluded.lang";
-
-        echo "Inserting data in main table...\n";
-        Yii::$app->db->createCommand($rawSql)->execute();
-        echo "Data insertion in main table is completed.\n";
-
-        Yii::$app->db->createCommand("DROP TABLE IF EXISTS $tmpTable")->execute();
-        echo "Temporary table dropped.\n";
-        echo "Task completed @ ".date('Y-m-d H:i:s')."\n";
+        $selectTmp = (new Query())->select($columnNames)->from($tmpTable);
+        $updateColumns = true;
+        $params = [];
+        Yii::$app->db->createCommand()->upsert($baseTable, $selectTmp, $updateColumns, $params)->execute();
+        
+        ImportHelper::afterImport($tmpTable);
+        
         return;
+    }
+
+    public function actionLoan($model='Loan', $file='loans.json')
+    {
+        if( isset($row['start_date']) )
+        {
+            $row['start_date'] = date('Y/m/d H:i:s', $row['start_date']);
+        }
+        if( isset($row['end_date']) )
+        {
+            $row['end_date'] = date('Y/m/d H:i:s', $row['end_date']);
+        }
+        if( isset($row['status']) )
+        {
+            $row['status'] = boolval(intval($row['status']));
+        }
+        return $row;
     }
 
     public function actionGenerate($limit=5, $fileName='users.json')
---
+++ helpers/ImportHelper.php
@@ -0,0 +1,126 @@
+<?php
+namespace app\helpers;
+
+use Yii;
+use \JsonMachine\JsonMachine;
+
+/**
+ * ImportHelper is a helper class which includes methods related to Imports/Import Commands.
+ *
+ * @author Tahir Raza <tahirraza.se@gmail.com>
+ * @since 2.0
+ */
+class ImportHelper
+{
+    /**
+     * [[streamInsert()]] function to fill table with JSON data via stream.
+     * @param string $model
+     * @param string $file
+     * @param string $table
+     * @param array $columnNames
+     * @return bool
+     */
+    public static function streamInsert($model, $file, $table, $columnNames)
+    {
+        // Read the json file with stream
+        $rows = JsonMachine::fromFile(Yii::$app->basePath.'/'.$file);
+        $userData = [];
+        foreach ($rows as $id => $row) {
+            ImportHelper::preProcess($model, $row);
+            if( count($userData) < 500){
+                $userData[] = $row;
+            } else {
+                // send a batch of 100 records for insertion
+                $insertCount = Yii::$app->db->createCommand()
+                    ->batchInsert(
+                        $table, $columnNames, $userData
+                    )
+                    ->execute();
+                // empty the userData array after insetion
+                // and insert the current user in the array as a first element
+                $userData = [];
+                $userData[] = $row;
+            }
+        }
+        
+        // insert the remaining data if any
+        if(count($userData) > 0)
+        {
+            $insertCount = Yii::$app->db->createCommand()
+                ->batchInsert(
+                    $table, $columnNames, $userData
+                )
+                ->execute();
+        }
+        return true;
+    }
+
+    /**
+     * [[beforeImport()]] function to prepare databse for import, creates temp table.
+     * @param string $model
+     * @return bool
+     */
+    public static function beforeImport($model)
+    {
+        $className = 'app\\models\\'.$model;
+        $modelInstance = new $className();
+
+        $columnSchema = [];
+        $columns = $modelInstance->getTableSchema()->columns;
+        foreach($columns as $column) {
+            $columnSchema[$column->name] = $column->type;
+        }
+        
+        // Create a temporary table just like passed model 'User' as default
+        $baseTable = $modelInstance->getTableSchema()->fullName;
+        $tmpTable =  'temp_'.$modelInstance->getTableSchema()->fullName;
+
+        // DROP Table IF EXISTS
+        // Overide the dropTable function to support IF EXISTS option
+        // otherwise, it will throw an error becuase table is not existing on first time
+        // Yii::$app->db->createCommand()->dropTable($tmpTable)->execute();
+        Yii::$app->db->createCommand("DROP TABLE IF EXISTS $tmpTable")->execute();
+        
+        /*
+         * TODO: Overide the function createTable 
+         * in order to support UNLOGGED, IF NOT EXISTS options
+         *
+         */
+        $rawSql = Yii::$app->db->createCommand()->createTable($tmpTable, $columnSchema)->getRawSql();
+        $rawSql = str_replace('CREATE TABLE', 'CREATE UNLOGGED TABLE IF NOT EXISTS ', $rawSql);
+        return Yii::$app->db->createCommand($rawSql)->execute();
+    }
+
+    /**
+     * [[afterImport()]] function to cleanup database once import is done.
+     * @param string $table
+     * @return bool
+     */
+    public static function afterImport($table)
+    {
+        return Yii::$app->db->createCommand("DROP TABLE IF EXISTS $table")->execute();
+    }
+
+    /**
+     * [[preProcess()]] process data before insertion.
+     * @param string $model
+     * @param array $row
+     * @return bool
+     */
+    public static function preProcess($model, &$row)
+    {
+        switch ($model) {
+            case 'Loan':
+                $row['start_date'] = date('Y/m/d H:i:s', $row['start_date']);
+                $row['end_date'] = date('Y/m/d H:i:s', $row['end_date']);
+                $row['status'] = boolval(intval($row['status']));
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+        
+        return true;
+    }
+}
